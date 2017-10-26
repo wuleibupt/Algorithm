69ce5eec1fe6e7bd0ef4a22830a935c72f9abc24
Your Name <you@example.com>
2017-10-26 15:11:08 +0800

    143. Reorder List

--- leetcode/reorderList.cpp
+++ leetcode/reorderList.cpp
@@ -3,23 +3,49 @@
 #include<vector>
 #include<unordered_set>
 using namespace std;
-class Solution
-{
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
 public:
-	bool wordBreak(string s, unordered_set<string> &dict) 
-	{
-		int len = s.size();
-		vector<bool> dp(len + 1, false);
-		dp[0] = true;
-		for (int pos = 0; pos < len; pos++)
+	void reorderList(ListNode* head) {
+		if (head == nullptr)
+			return;
+		ListNode* fast;
+		ListNode* slow;
+		fast = head->next;
+		slow = head;
+		while (fast != nullptr && fast->next != nullptr)
 		{
-			for (int i = pos; dp[pos] && i < len; ++i)
-			{
-				if (dict.find(s.substr(pos, i - pos + 1)) != dict.end())
-					dp[i + 1] = true;
-			}
+			fast = fast->next->next;
+			slow = slow->next;
 		}
-		return dp[len];
+		ListNode* pre = slow->next;
+		if (pre == nullptr)return;
+		ListNode* nex = pre->next;
+		while (nex != nullptr)
+		{
+			pre->next = nex->next;
+			nex->next = slow->next;
+			slow->next = nex;
+			nex = pre->next;
+		}
+		mergeNode1(head, slow);
+	}
+	void mergeNode1(ListNode* head1, ListNode* head2)
+	{
+		ListNode* p = head1;
+		ListNode* q = head2->next;
+		while (q != nullptr && p != nullptr)
+		{
+			head2->next = q->next;
+			q->next = p->next;
+			p->next = q;
+			q = head2->next;
+			p = p->next->next;
+		}
 	}
 };
 
